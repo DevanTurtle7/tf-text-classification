81a4053841381df395198f8ed53c843680b2bfc8
Devan Kavalchek <devankav7@gmail.com>
2023-02-27 16:24:39 -0500

    Trained model

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import tensorflow as tf
 
+
 def download_test_data():
   url = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
   dataset = tf.keras.utils.get_file("aclImdb_v1", url, untar=True, cache_dir='.', cache_subdir='')
@@ -11,8 +12,10 @@ def download_test_data():
   remove_dir = os.path.join(train_dir, 'unsup')
   shutil.rmtree(remove_dir)
 
+
 def main():
   download_test_data()
+
 
 if __name__ == "__main__":
   main()
