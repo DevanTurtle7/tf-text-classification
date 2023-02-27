22f46e56052b48f51dd6088238d5c4bd5a8bb1a3
Devan Kavalchek <devankav7@gmail.com>
2023-02-27 17:04:54 -0500

    Updated cli

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ from train import custom_standardization
 def main():
   model = tf.keras.models.load_model('saved_model')
   print('\nEnter a comment to be analyzed. Press enter to quit.')
+  print('\n> ', end='')
   response = input()
 
   while response != '':
     print(model.predict([response]))
+    print('\n> ', end='')
     response = input()
 
 
